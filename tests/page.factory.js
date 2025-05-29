b0684607729b80feeb6e13136e20f2edd6d0a906
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-05-29 17:42:17 -0400

    feat: add support for SecurePage in PageFactory

--- tests/page.factory.js
+++ tests/page.factory.js
@@ -1,11 +1,13 @@
 const LoginPage = require('./pages/login.page');
+const SecurePage = require('./pages/secure.page');
 
 class PageFactory {
   static createPage(pageName, driver) {
     switch(pageName) {
       case 'login':
         return new LoginPage(driver);
-      // Agrega más páginas aquí si quieres
+      case 'secure':
+        return new SecurePage(driver);
       default:
         throw new Error(`Page not found: ${pageName}`);
     }
