d10b161e36b0e158803d62e739bef285b7413be8
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-05-29 09:45:07 -0400

    feat: add SecurePage class for handling secure page interactions

---
+++ tests/pages/securePage.js
@@ -0,0 +1,20 @@
+const { By } = require('selenium-webdriver');
+
+class SecurePage {
+  constructor(driver) {
+    this.driver = driver;
+    this.logoutButton = By.css('a.button.secondary.radius');
+    this.flashMessage = By.id('flash');
+  }
+
+  async logout() {
+    await this.driver.findElement(this.logoutButton).click();
+    return new LoginPage(this.driver);
+  }
+
+  async getFlashMessage() {
+    return await this.driver.findElement(this.flashMessage).getText();
+  }
+}
+
+module.exports = SecurePage;
